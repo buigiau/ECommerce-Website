51511d52106b9ab8eb9632860da02c909f6e7f51
sacsigiau <sacsigiau@gmail.com>
2024-09-29 09:28:56 +0700

    update count to cart

--- ECommerceMVC/Program.cs
+++ ECommerceMVC/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<Hshop2023Context>(options =>
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
-	options.IdleTimeout = TimeSpan.FromSeconds(10);
+	options.IdleTimeout = TimeSpan.FromMinutes(10);
 	options.Cookie.HttpOnly = true;
 	options.Cookie.IsEssential = true;
 });
---
+++ ECommerceMVC/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceMVC.Helpers;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+namespace ECommerceMVC.ViewComponents
+{
+	public class CartViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke ()
+		{
+			var cart = HttpContext.Session.Get<List<CartItem>>(MySetting.CART_KEY) ?? new List<CartItem>();
+			return View("CartPenal", new CartModel
+			{
+				Quantity = cart.Sum(p  => p.SoLuong),
+				Total = cart.Sum(p => p.ThanhTien),
+			});
+		}
+	}
+}
