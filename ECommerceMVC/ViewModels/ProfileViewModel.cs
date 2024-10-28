45e27db26ca6a685edf2b90ab021b9966be6d156
buigiau <sacsigiau@gmail.com>
2024-10-28 22:19:29 +0700

    adjustview

---
+++ ECommerceMVC/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceMVC.ViewModels
+{
+	public class ProfileViewModel
+	{
+		public string HoTen { get; set; }
+		public DateTime NgaySinh { get; set; }
+		public string Email { get; set; }
+		public string DienThoai { get; set; }
+	}
+
+}
